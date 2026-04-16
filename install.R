95a27ea8897818fb13651a49baf2febec9d28048
Matt Dalle Piagge <47819526+mattjbr123@users.noreply.github.com>
2026-04-16 23:29:58 +0100

    Add additional R packages for installation

--- install.R
+++ install.R
@@ -2,6 +2,6 @@
 # Binder runs this automatically at environment build time
 
 install.packages(
-  c("ncdf4", "zarr"),
+  c("ncdf4", "zarr, "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate"),
   repos = "https://packagemanager.posit.co/cran/2024-01-01"
 )
