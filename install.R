af07511c1aead8fce7aa901ef0a64536d3a9ca8a
Matt Dalle Piagge <47819526+mattjbr123@users.noreply.github.com>
2026-04-17 22:47:44 +0100

    Enhance install.packages with repository and options
    Added repository and options for package installation.

--- install.R
+++ install.R
@@ -2,4 +2,4 @@
 # Binder runs this automatically at environment build time
 
 install.packages(
-  c("ncdf4", "zarr", "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate"))
+  c("ncdf4", "zarr", "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate"), repos = "https://cloud.r-project.org", verbose=TRUE, keep_output=TRUE)
