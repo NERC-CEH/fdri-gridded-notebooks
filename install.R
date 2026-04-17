fbda74176671a20374e6f66bcfc66412ff62611b
Matt Dalle Piagge <47819526+mattjbr123@users.noreply.github.com>
2026-04-17 22:36:05 +0100

    Fix syntax error in package installation list

--- install.R
+++ install.R
@@ -2,4 +2,4 @@
 # Binder runs this automatically at environment build time
 
 install.packages(
-  c("ncdf4", "zarr, "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate"))
+  c("ncdf4", "zarr", "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate"))
