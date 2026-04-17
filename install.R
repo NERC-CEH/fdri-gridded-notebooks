ed59984678a7104cbc26ce8181ae90698ada9b59
Matt Dalle Piagge <47819526+mattjbr123@users.noreply.github.com>
2026-04-17 22:04:46 +0100

    Modify package installation in install.R
    Updated the install.packages call to remove the repos argument.

--- install.R
+++ install.R
@@ -2,6 +2,4 @@
 # Binder runs this automatically at environment build time
 
 install.packages(
-  c("ncdf4", "zarr, "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate"),
-  repos = "https://packagemanager.posit.co/cran/2024-01-01"
-)
+  c("ncdf4", "zarr, "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate"))
