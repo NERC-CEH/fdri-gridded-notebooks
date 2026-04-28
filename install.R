093110674f0224d5f6febf1d67ce3b4233ae8292
Matt Dalle Piagge <47819526+mattjbr123@users.noreply.github.com>
2026-04-28 15:28:11 +0100

    Add 'bit64' package to installation list

--- install.R
+++ install.R
@@ -2,4 +2,4 @@
 # Binder runs this automatically at environment build time
 
 install.packages(
-  c("ncdf4", "zarr", "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate"), repos = "https://cloud.r-project.org", verbose=TRUE, keep_output=TRUE)
+  c("ncdf4", "zarr", "curl", "ggplot2", "dplyr", "tidyr", "sf", "rnaturalearth", "rnaturalearthdata", "scales", "lubridate", "bit64"), repos = "https://cloud.r-project.org", verbose=TRUE, keep_output=TRUE)
